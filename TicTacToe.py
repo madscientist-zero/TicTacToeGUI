2f33e505ce20cb280f7e0a64984c5d39af28bdec
MadScientist <57386559+madscientist-zero@users.noreply.github.com>
2019-12-14 05:39:23 +0700

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,116 +1,161 @@
 from tkinter import *
 import tkinter.messagebox
-tk = Tk()
-tk.title("Tic Tac Toe MadScientist Game")
-
-pa = StringVar()
-playerb = StringVar()
-p1 = StringVar()
-p2 = StringVar()
-
-player1_name = Entry(tk, textvariable=p1, bd=10)
-player1_name.grid(row=4, column=1, columnspan=16)
-player2_name = Entry(tk, textvariable=p2, bd=10)
-player2_name.grid(row=4, column=1, columnspan=16)
-
-bclick = True
-flag = 0
-
-def disableButton():
-    button1.configure(state=DISABLED)
-    button2.configure(state=DISABLED)
-    button3.configure(state=DISABLED)
-    button4.configure(state=DISABLED)
-    button5.configure(state=DISABLED)
-    button6.configure(state=DISABLED)
-    button7.configure(state=DISABLED)
-    button8.configure(state=DISABLED)
-    button9.configure(state=DISABLED)
+import tkinter
+import random
 
 
+class play(object):
+    
+    block_size = 100
+    def __init__(self, parent):
+        parent.title('Tic Tac Toe GUI')
+        self.parent = parent
 
-def btnClick(buttons):
-    global bclick, flag, player2_name, player1_name, playerb, pa
-    if buttons["text"] == " " and bclick == True:
-        buttons["text"] = "X"
-        bclick = False
-        playerb = p2.get() + " you are winner!"
-        pa = p1.get() + " You are winner!"
-        checkForWin()
-        flag += 1
+        self.initialize_game()
 
+    def initialize_game(self):
+        self.board = [None, None, None, None, None, None, None, None, None]
+        self.map = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 3, (1, 1): 4, (1, 2): 5, (2, 0): 6, (2, 1): 7,
+                    (2, 2): 8}
+        self.top_frame = tkinter.Frame(self.parent)
+        self.top_frame.pack(side=tkinter.TOP)
 
-    elif buttons["text"] == " " and bclick == False:
-        buttons["text"] = "O"
-        bclick = True
-        checkForWin()
-        flag += 1
-    else:
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", "already Clicked bro!")
+        restart_button = tkinter.Button(self.top_frame, text='New Game', width=30, height=3,
+                                        command=self.restart)
+        restart_button.pack()
 
-def checkForWin():
-    if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
-        button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X' or
-        button7['text'] =='X' and button8['text'] == 'X' and button9['text'] == 'X' or
-        button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X' or
-        button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X' or
-        button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
-        button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
-        button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
-        button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
-        disableButton()
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", pa)
+        self.bottom_frame=tkinter.Frame(self.parent)
+        self.bottom_frame.pack(side=tkinter.BOTTOM)
 
-    elif(flag == 8):
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", "It's a Tie")
+        self.my_lbl=tkinter.Label(self.bottom_frame,text=None)
+        self.my_lbl.pack()
 
-    elif (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
-          button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
-          button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
-          button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O' or
-          button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O' or
-          button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
-          button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
-          button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
-          button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
-        disableButton()
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", playerb)
+        self.canvas = tkinter.Canvas(self.top_frame,
+                                     width=self.block_size * 3,
+                                     height=self.block_size * 3)
 
+        for ro in range(3):
+            for col in range(3):
 
-buttons = StringVar()
+                self.canvas.create_rectangle(self.block_size * col,
+                                             self.block_size * ro,
+                                             self.block_size * (col + 1),
+                                             self.block_size * (ro + 1),fill='white')
 
-label = Label( tk, text="Player 1:", font='Impact 20 bold', bg='green', fg='yellow', height=1, width=8)
-label.grid(row=1, column=0)
+        self.canvas.bind("<Button-1>", self.play)
+        self.canvas.pack()
+
+    def board_full(self):
+        if None not in self.board:
+            return True
+        else:
+            return False
 
 
-label = Label( tk, text="Player 2:", font='Impact 20 bold', bg='green', fg='yellow', height=1, width=8)
-label.grid(row=2, column=0)
+    def possible_moves(self):
+        """return: list of possible moves"""
+        possible_moves = []
+        for i in range(0, 9):
+            if self.board[i] is None:
+                possible_moves.append(i)
+            else:
+                pass
+        return possible_moves
 
-button1 = Button(tk, text=" ", font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button1))
-button1.grid(row=3, column=0)
+    def pc_move(self):
+        m = True
+        while m:
+            pc_move = random.randint(0, 8)
+            if pc_move in self.possible_moves():
+                self.board[pc_move] = 'O'
+                self.canvas.itemconfigure(tagOrId=(pc_move+1),fill='blue')
+                m = False
+            else:
+                continue
+        return self
 
-button2 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button2))
-button2.grid(row=3, column=1)
+    def draw_out(self):
+        """to be deleted"""
+        print(self.board[0:3])
+        print(self.board[3:6])
+        print(self.board[6:9])
 
-button3 = Button(tk, text=' ',font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button3))
-button3.grid(row=3, column=2)
+    def play(self, event):
+        """
+        when the player clicks on a un-taken square, this method first translate cursor into cell number,
+        then update game board and check game result based on condition
+        :parameter: click
+        :return: updated game object
+        """
+        print('clicked', event.y, event.x)
+        cx = self.canvas.canvasx(event.x)
+        cy = self.canvas.canvasy(event.y)
+        cid = self.canvas.find_closest(cx,cy)[0]
+        my_move = self.map[(cy // self.block_size, cx // self.block_size)]
+        if self.board[my_move] is None:
+            self.board[my_move] = 'X'
+            self.canvas.itemconfigure(cid,fill='green') # green color for player
+        else:
+            return None
 
-button4 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button4))
-button4.grid(row=4, column=0)
+        self.draw_out()
+        if self.check_game()is not None:
+            print(self.check_game())
+        else:
+            pass
+        self.possible_moves()
+        self.pc_move()                            
+        self.draw_out()                          
+        if self.check_game()is not None:
+            print(self.check_game()) 
+        else:
+            pass
+        return self
 
-button5 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button5))
-button5.grid(row=4, column=1)
+    def check_game(self):
+        """
+        Check if the game is win or lost or a tie
+        Return:  win, lose, tie, none """
+        result=None
+        if (self.board[0] == self.board[1] == self.board[2] == 'X') or (
+                            self.board[3] == self.board[4] == self.board[5] == 'X') or (
+                            self.board[6] == self.board[7] == self.board[8] == 'X') or (
+                            self.board[0] == self.board[3] == self.board[6] == 'X') or (
+                            self.board[1] == self.board[4] == self.board[7] == 'X') or (
+                            self.board[2] == self.board[5] == self.board[8] == 'X') or (
+                            self.board[0] == self.board[4] == self.board[8] == 'X') or (
+                            self.board[2] == self.board[4] == self.board[6] == 'X'):
+            result = 'You win!'
+            self.my_lbl.configure(text=result)
+        elif (self.board[0] == self.board[1] == self.board[2] == 'O') or (
+                            self.board[3] == self.board[4] == self.board[5] == 'O') or (
+                            self.board[6] == self.board[7] == self.board[8] == 'O') or (
+                            self.board[0] == self.board[3] == self.board[6] == 'O') or (
+                            self.board[1] == self.board[4] == self.board[7] == 'O') or (
+                            self.board[2] == self.board[5] == self.board[8] == 'O') or (
+                            self.board[0] == self.board[4] == self.board[8] == 'O') or (
+                            self.board[2] == self.board[4] == self.board[6] == 'O'):
+            result = 'You lost!'
+            self.my_lbl.config(text=result)
+        elif self.board_full()is True:
+                result = 'A tie!'
+                self.my_lbl.configure(text=result)
+        else:
+            pass
+        return result
 
-button6 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button6))
-button6.grid(row=4, column=2)
 
-button7 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button7))
-button7.grid(row=5, column=0)
+    def restart(self):
+        
+        self.top_frame.destroy()
+        self.bottom_frame.destroy()
+        self.initialize_game()
 
-button8 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button8))
-button8.grid(row=5, column=1)
 
-button9 = Button(tk, text=' ', font='Times 20 bold', bg='purple', fg='white', height=4, width=8, command=lambda: btnClick(button9))
-button9.grid(row=5, column=2)
+def main():
+    root = tkinter.Tk()
+    the_game = play(root)
+    root.mainloop()
 
-tk.mainloop()
+if __name__ == '__main__':
+    main()
