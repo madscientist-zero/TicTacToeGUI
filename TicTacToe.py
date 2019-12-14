bbe026b0df6406ab3a8d5deffc92c72518c8f2a6
MadScientist <57386559+madscientist-zero@users.noreply.github.com>
2019-12-14 17:48:28 +0700

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,161 +1,209 @@
-from tkinter import *
-import tkinter.messagebox
-import tkinter
-import random
+from tkinter import Tk,ttk,Button
+from tkinter import messagebox
+from random import randint
+
+ActivePlayer = 1
+p1 = []
+p2 = []
+mov = 0
+
+def SetLayout(id,player_symbol):
+    if id==1:
+        b1.config(text= player_symbol)
+        b1.state(['disabled'])
+    elif id==2:
+        b2.config(text= player_symbol)
+        b2.state(['disabled'])
+    elif id==3:
+        b3.config(text= player_symbol)
+        b3.state(['disabled'])
+    elif id==4:
+        b4.config(text= player_symbol)
+        b4.state(['disabled'])
+    elif id==5:
+        b5.config(text= player_symbol)
+        b5.state(['disabled'])
+    elif id==6:
+        b6.config(text= player_symbol)
+        b6.state(['disabled'])
+    elif id==7:
+        b7.config(text= player_symbol)
+        b7.state(['disabled'])
+    elif id==8:
+        b8.config(text= player_symbol)
+        b8.state(['disabled'])
+    elif id==9:
+        b9.config(text= player_symbol)
+        b9.state(['disabled'])
+
+def CheckWinner():
+    global mov 
+    winner = -1
+
+    if(1 in p1) and (2 in p1) and (3 in p1):
+        winner = 1
+    if(1 in p2) and (2 in p2) and (3 in p2):
+        winner = 2
+
+    if(4 in p1) and (5 in p1) and (6 in p1):
+        winner = 1
+    if(4 in p2) and (5 in p2) and (6 in p2):
+        winner = 2
+
+    if(7 in p1) and (8 in p1) and (9 in p1):
+        winner = 1
+    if(7 in p2) and (8 in p2) and (9 in p2):
+        winner = 2
+
+    if(1 in p1) and (4 in p1) and (7 in p1):
+        winner = 1
+    if(1 in p2) and (4 in p2) and (7 in p2):
+        winner = 2
+
+    if(2 in p1) and (5 in p1) and (8 in p1):
+        winner = 1
+    if(2 in p2) and (5 in p2) and (8 in p2):
+        winner = 2
+
+    if(3 in p1) and (6 in p1) and ( 9 in p1):
+        winner = 1
+    if(3 in p2) and (6 in p2) and (9 in p2):
+        winner = 2
+
+    if(1 in p1) and (5 in p1) and ( 9 in p1):
+        winner = 1
+    if(1 in p2) and (5 in p2) and (9 in p2):
+        winner = 2
+
+    if(3 in p1) and (5 in p1) and ( 7 in p1):
+        winner = 1
+    if(3 in p2) and (5 in p2) and (7 in p2):
+        winner = 2
+
+    if winner ==1:
+        messagebox.showinfo(title="Congratulations.", 
+            message="Player 1 is the winner")
+    elif winner ==2:
+        messagebox.showinfo(title="Congratulations.", 
+            message="Player 2 is the winner")
+    elif mov ==9:
+        messagebox.showinfo(title="Draw", 
+            message="It's a Draw!!")
+
+def ButtonClick(id):
+    global ActivePlayer
+    global p1,p2
+    global mov
+
+    if(ActivePlayer ==1):
+        SetLayout(id,"X")
+        p1.append(id)
+        mov +=1
+        root.title("Tic Tac Toe : Player 2")
+        ActivePlayer =2
+
+    elif(ActivePlayer==2):
+        SetLayout(id,"O")
+        p2.append(id)
+        mov +=1
+        root.title("Tic Tac Toe : Player 1")
+        ActivePlayer =1
+    CheckWinner()
+
+def AutoPlay():
+    global p1; global p2
+    Empty = []
+    for cell in range(9):
+        if(not((cell +1 in p1) or (cell +1 in p2))):
+            Empty.append(cell+1)
+    try:
+        RandIndex = randint(0,len(Empty) -1)
+        ButtonClick(Empty[RandIndex])
+    except:
+        pass
+
+def EnableAll():
+    b1.config(text= " ")
+    b1.state(['!disabled'])
+    b2.config(text= " ")
+    b2.state(['!disabled'])
+    b3.config(text= " ")
+    b3.state(['!disabled'])
+    b4.config(text= " ")
+    b4.state(['!disabled'])
+    b5.config(text= " ")
+    b5.state(['!disabled'])
+    b6.config(text= " ")
+    b6.state(['!disabled'])
+    b7.config(text= " ")
+    b7.state(['!disabled'])
+    b8.config(text= " ")
+    b8.state(['!disabled'])
+    b9.config(text= " ")
+    b9.state(['!disabled'])
 
 
-class play(object):
-    
-    block_size = 100
-    def __init__(self, parent):
-        parent.title('Tic Tac Toe GUI')
-        self.parent = parent
-
-        self.initialize_game()
-
-    def initialize_game(self):
-        self.board = [None, None, None, None, None, None, None, None, None]
-        self.map = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 3, (1, 1): 4, (1, 2): 5, (2, 0): 6, (2, 1): 7,
-                    (2, 2): 8}
-        self.top_frame = tkinter.Frame(self.parent)
-        self.top_frame.pack(side=tkinter.TOP)
-
-        restart_button = tkinter.Button(self.top_frame, text='New Game', width=30, height=3,
-                                        command=self.restart)
-        restart_button.pack()
-
-        self.bottom_frame=tkinter.Frame(self.parent)
-        self.bottom_frame.pack(side=tkinter.BOTTOM)
-
-        self.my_lbl=tkinter.Label(self.bottom_frame,text=None)
-        self.my_lbl.pack()
-
-        self.canvas = tkinter.Canvas(self.top_frame,
-                                     width=self.block_size * 3,
-                                     height=self.block_size * 3)
-
-        for ro in range(3):
-            for col in range(3):
-
-                self.canvas.create_rectangle(self.block_size * col,
-                                             self.block_size * ro,
-                                             self.block_size * (col + 1),
-                                             self.block_size * (ro + 1),fill='white')
-
-        self.canvas.bind("<Button-1>", self.play)
-        self.canvas.pack()
-
-    def board_full(self):
-        if None not in self.board:
-            return True
-        else:
-            return False
+def Restart():
+    global p1,p2,mov,ActivePlayer
+    p1.clear(); p2.clear()
+    mov,ActivePlayer = 0,1
+    root.title("Tic Tac Toe : Player 1")
+    EnableAll()
 
 
-    def possible_moves(self):
-        """return: list of possible moves"""
-        possible_moves = []
-        for i in range(0, 9):
-            if self.board[i] is None:
-                possible_moves.append(i)
-            else:
-                pass
-        return possible_moves
-
-    def pc_move(self):
-        m = True
-        while m:
-            pc_move = random.randint(0, 8)
-            if pc_move in self.possible_moves():
-                self.board[pc_move] = 'O'
-                self.canvas.itemconfigure(tagOrId=(pc_move+1),fill='blue')
-                m = False
-            else:
-                continue
-        return self
-
-    def draw_out(self):
-        """to be deleted"""
-        print(self.board[0:3])
-        print(self.board[3:6])
-        print(self.board[6:9])
-
-    def play(self, event):
-        """
-        when the player clicks on a un-taken square, this method first translate cursor into cell number,
-        then update game board and check game result based on condition
-        :parameter: click
-        :return: updated game object
-        """
-        print('clicked', event.y, event.x)
-        cx = self.canvas.canvasx(event.x)
-        cy = self.canvas.canvasy(event.y)
-        cid = self.canvas.find_closest(cx,cy)[0]
-        my_move = self.map[(cy // self.block_size, cx // self.block_size)]
-        if self.board[my_move] is None:
-            self.board[my_move] = 'X'
-            self.canvas.itemconfigure(cid,fill='green') # green color for player
-        else:
-            return None
-
-        self.draw_out()
-        if self.check_game()is not None:
-            print(self.check_game())
-        else:
-            pass
-        self.possible_moves()
-        self.pc_move()                            
-        self.draw_out()                          
-        if self.check_game()is not None:
-            print(self.check_game()) 
-        else:
-            pass
-        return self
-
-    def check_game(self):
-        """
-        Check if the game is win or lost or a tie
-        Return:  win, lose, tie, none """
-        result=None
-        if (self.board[0] == self.board[1] == self.board[2] == 'X') or (
-                            self.board[3] == self.board[4] == self.board[5] == 'X') or (
-                            self.board[6] == self.board[7] == self.board[8] == 'X') or (
-                            self.board[0] == self.board[3] == self.board[6] == 'X') or (
-                            self.board[1] == self.board[4] == self.board[7] == 'X') or (
-                            self.board[2] == self.board[5] == self.board[8] == 'X') or (
-                            self.board[0] == self.board[4] == self.board[8] == 'X') or (
-                            self.board[2] == self.board[4] == self.board[6] == 'X'):
-            result = 'You win!'
-            self.my_lbl.configure(text=result)
-        elif (self.board[0] == self.board[1] == self.board[2] == 'O') or (
-                            self.board[3] == self.board[4] == self.board[5] == 'O') or (
-                            self.board[6] == self.board[7] == self.board[8] == 'O') or (
-                            self.board[0] == self.board[3] == self.board[6] == 'O') or (
-                            self.board[1] == self.board[4] == self.board[7] == 'O') or (
-                            self.board[2] == self.board[5] == self.board[8] == 'O') or (
-                            self.board[0] == self.board[4] == self.board[8] == 'O') or (
-                            self.board[2] == self.board[4] == self.board[6] == 'O'):
-            result = 'You lost!'
-            self.my_lbl.config(text=result)
-        elif self.board_full()is True:
-                result = 'A tie!'
-                self.my_lbl.configure(text=result)
-        else:
-            pass
-        return result
 
 
-    def restart(self):
-        
-        self.top_frame.destroy()
-        self.bottom_frame.destroy()
-        self.initialize_game()
+root = Tk()
+root.title("Tic Tac toe : Player 1")
+st = ttk.Style()
+st.configure("my.TButton", font=('Chiller',24,'bold'))
+
+b1 = ttk.Button(root, text=" ", style="my.TButton")
+b1.grid(row=0, column=0, sticky="snew", ipadx=40,ipady=40)
+b1.config(command = lambda : ButtonClick(1))
 
 
-def main():
-    root = tkinter.Tk()
-    the_game = play(root)
-    root.mainloop()
+b2 = ttk.Button(root, text=" ",style ="my.TButton")
+b2.grid(row=0, column=1, sticky="snew", ipadx=40,
+        ipady=40)
+b2.config(command = lambda : ButtonClick(2))
 
-if __name__ == '__main__':
-    main()
+b3= ttk.Button(root, text=" ",style="my.TButton")
+b3.grid(row=0, column=2, sticky="snew", ipadx=40,
+        ipady=40)
+b3.config(command = lambda : ButtonClick(3))
+
+b4 = ttk.Button(root, text=" ",style="my.TButton")
+b4.grid(row=1, column=0, sticky="snew", ipadx=40,
+        ipady=40)
+b4.config(command = lambda : ButtonClick(4))
+
+b5 = ttk.Button(root, text=" ",style="my.TButton")
+b5.grid(row=1, column=1, sticky="snew", ipadx=40,
+        ipady=40)
+b5.config(command = lambda : ButtonClick(5))
+
+b6 = ttk.Button(root, text=" ",style="my.TButton")
+b6.grid(row=1, column=2, sticky="snew", ipadx=40,
+        ipady=40)
+b6.config(command = lambda : ButtonClick(6))
+
+b7 = ttk.Button(root, text=" ",style="my.TButton")
+b7.grid(row=2, column=0, sticky="snew", ipadx=40,
+        ipady=40)
+b7.config(command = lambda : ButtonClick(7))
+
+b8 = ttk.Button(root, text=" ",style="my.TButton")
+b8.grid(row=2, column=1, sticky="snew", ipadx=40,
+        ipady=40)
+b8.config(command = lambda : ButtonClick(8))
+
+b9 = ttk.Button(root, text=" ",style="my.TButton")
+b9.grid(row=2, column=2, sticky="snew", ipadx=40,
+        ipady=40)
+b9.config(command = lambda : ButtonClick(9))
+
+Button(text="New Game..", font=('Papyrus', 22, 'bold'), bg='Purple', fg='white',
+            border=5, width=4,command = lambda :Restart()).grid(row=4, column=1, sticky="we")
+
+root.mainloop()
